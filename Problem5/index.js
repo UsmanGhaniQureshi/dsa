52587c835a4cd0bbda4a576aa9f498e1d212a64c
Usman Ghani Qureshi <musmanghani2000@gmail.com>
2022-10-27 14:49:19 +0500

    working Solution added Problem 5

--- Problem5/index.js
+++ Problem5/index.js
@@ -1,29 +1,63 @@
+// var lengthOfLongestSubstring = (str) => {
+//   let count = 0;
+//   let maxCount = 0;
+//   let obj = {};
+//   for (let ch of str) {
+//     if (!obj[ch]) {
+//       count++;
+//       obj[ch] = ch;
+//     } else {
+//       obj = {
+//         [ch]: ch,
+//       };
+//       count = 1;
+//     }
+//     maxCount = Math.max(count, maxCount);
+//   }
+//   console.log(maxCount);
+// };
+
+// var lengthOfLongestSubstring = (str) => {
+//   let maxCount = 0;
+//   let obj = {};
+//   let maxArr = [];
+//   for (let ch of str) {
+//     if (!obj[ch]) {
+//       obj[ch] = ch;
+//       maxCount++;
+//     } else {
+//       maxArr.push(Object.keys(obj));
+//       obj = {
+//         [ch]: ch,
+//       };
+//     }
+//   }
+
+//   let maxLength = 0;
+
+//   for (let i = 0; i < maxArr.length; i++) {
+//     maxLength = Math.max(maxLength, maxArr[i].length);
+//   }
+
+//   console.log(maxLength);
+// };
+
 var lengthOfLongestSubstring = (str) => {
-  let maxCount = 0;
-  let obj = {};
-  let maxArr = [];
-  for (let ch of str) {
-    if (!obj[ch]) {
-      obj[ch] = ch;
-      maxCount++;
-    } else {
-      maxArr.push(Object.keys(obj));
-      obj = {
-        [ch]: ch,
-      };
-    }
-  }
-
+  let output = "";
   let maxLength = 0;
-
-  for (let i = 0; i < maxArr.length; i++) {
-    maxLength = Math.max(maxLength, maxArr[i].length);
+  for (let ch of str) {
+    let index = output.indexOf(ch);
+    if (index === -1) {
+      output += ch;
+    } else {
+      output = output.substring(index + 1, output.length) + ch;
+    }
+    maxLength = Math.max(maxLength, output.length);
   }
-
   console.log(maxLength);
 };
 
-lengthOfLongestSubstring("pwwkew");
+lengthOfLongestSubstring("dvdf");
 
 // obj ={}
 // obj ={a}
@@ -35,3 +69,9 @@ lengthOfLongestSubstring("pwwkew");
 // obj ={a,b,c}
 // obj ={a,b,c} => must store at some place now newObj= {b}
 // obj ={b} => must store at some place newObj={b}
+
+// "abcabcbb"
+// output =""
+// output ="a"
+// output ="ab"
+// output ="abc"
